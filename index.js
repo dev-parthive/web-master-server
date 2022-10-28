a0977a976a5e137dd76a15c5c1fe5531e3887748
dev-parthive <dev-parthive>
2022-10-28 09:16:48 +0600

    some change in json data

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 const port = 5000;
+const courses = require("./categories.json")
 app.get("/", (req, res) =>{
     res.send("server is running on port 5000")
 })
 
+app.get("/courses", (req, res) =>{
+    res.send(courses)
+})
 
 
 app.listen(port, ()=>{
