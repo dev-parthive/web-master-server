ecd104807f03235cecb9ad1d45ccb815d3353f32
dev-parthive <dev-parthive>
2022-10-28 10:32:37 +0600

    some change in json file

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.get("/", (req, res) =>{
 app.get("/courses", (req, res) =>{
     res.send(courses)
 })
-
+app.get('course/:id', (req, res) =>{
+    // const id = req.params.id;
+    // const selectCourse = courses.filter(course => course._id === id)
+    // res.send(selectCourse)
+    console.log(req.params.id)
+})
 
 app.listen(port, ()=>{
     console.log("server is running on port ", port)
